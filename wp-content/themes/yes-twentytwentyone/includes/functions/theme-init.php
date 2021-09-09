2d7eb59638062280fe4e8bfb727914de2be27269
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2021-09-09 17:54:23 +0800

    Yes.my 2021 theme setup

---
+++ wp-content/themes/yes-twentytwentyone/includes/functions/theme-init.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * Function to enqueue the stylesheets and javascripts files
+ * 
+ * @since    1.0.0
+ * 
+ * @param    array      $plans      Object array of plans which can be retrieved from the database - get_option('genapi_plans_data')
+ * @return   array
+ */
+if (!function_exists('yes_enqueue_scripts')) {
+    function yes_enqueue_scripts()
+    {
+        wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.1.0');
+        wp_enqueue_style('yes-styles', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0');
+        wp_enqueue_style('yes-styles-responsive', get_template_directory_uri() . '/assets/css/responsive.css', array(), '1.0.0');
+
+        wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/jquery.min.js', array(), '3.5.1', true);
+        wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.js', array(), '5.1.0', true);
+        wp_enqueue_script('iconify', get_template_directory_uri() . '/assets/js/iconify.min.js', array(), '2.0', true);
+    }
+    add_action('wp_enqueue_scripts', 'yes_enqueue_scripts');
+}
+
+
+/**
+ * Function to change the logo class when using the 'the_custom_logo()' function to display logo image
+ * 
+ * @since    1.0.0
+ * 
+ * @param    string     $html      The default HTML for the the_custom_logo() function
+ * @return   string     Returning the manipulated HTML string
+ */
+if (!function_exists('change_logo_class')) {
+    function change_logo_class($html)
+    {
+        $html = str_replace('custom-logo', 'navbar-brand', $html);
+        $html = str_replace('custom-logo-link', 'navbar-brand', $html);
+        return $html;
+    }
+    add_filter('get_custom_logo', 'change_logo_class');
+}
+
+
+/**
+ * Function for Yes.my TwentyTwentyOne theme setup
+ * 
+ * @since    1.0.0
+ */
+if (!function_exists('yes_twentytwentyone_setup')) {
+    function yes_twentytwentyone_setup()
+    {
+        /** To add the theme support for custom logo */
+        add_theme_support(
+            'custom-logo',
+            array(
+                'height'               => 52,
+                'width'                => 100,
+                'flex-width'           => true,
+                'flex-height'          => true,
+                'unlink-homepage-logo' => false,
+            )
+        );
+    }
+    add_action('after_setup_theme', 'yes_twentytwentyone_setup');
+}
+
+
+/**
+ * Function to display custom logo in custom markup
+ * 
+ * @since    1.0.0
+ */
+if (!function_exists('display_yes_logo')) {
+    function display_yes_logo()
+    {
+        $custom_logo_id = get_theme_mod('custom_logo');
+        $logo       = wp_get_attachment_image_src($custom_logo_id, 'full');
+        $site_url   = site_url();
+
+        if (has_custom_logo()) {
+            echo '<a href="' . $site_url . '" class="navbar-brand"><img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '" class="logo-top" /></a>';
+        } else {
+            echo '<h1><a href="' . $site_url . '">' . get_bloginfo('name') . '</a></h1>';
+        }
+    }
+}
+
+/**
+ * Function to register custom menus for yes.my theme
+ * 
+ * @since    1.0.0
+ */
+if (!function_exists('register_menus')) {
+    function register_menus()
+    {
+        if (function_exists('register_nav_menus')) {
+            register_nav_menus(
+                array(
+                    'primary'           => esc_html__('Primary', 'yes.my'),
+                    'shop-mobile-plans' => esc_html__('Mobile Plans', 'yes.my'),
+                    'shop-broadband'    => esc_html__('Broadband', 'yes.my'),
+                    'shop-existing-customers' => esc_html__('Existing Customers', 'yes.my'),
+
+                    'footer-column-1'   => esc_html__('Footer - Column 1', 'yes.my'),
+                    'footer-column-2'   => esc_html__('Footer - Column 2', 'yes.my'),
+                    'footer-column-3'   => esc_html__('Footer - Column 3', 'yes.my'),
+                    'footer-column-4'   => esc_html__('Footer - Column 4', 'yes.my'),
+                    'footer-column-5'   => esc_html__('Footer - Column 5', 'yes.my')
+                )
+            );
+        }
+    }
+    add_action('init', 'register_menus');
+}
+
+
+/**
+ * Function to add args in wp_nav_menu() to add custom class in <li>
+ * 
+ * @since    1.0.0
+ */
+if (!function_exists('nav_add_li_class')) {
+    function nav_add_li_class($classes, $item, $args)
+    {
+        if (property_exists($args, 'li_class')) {
+            $classes[] = $args->li_class;
+        }
+        return $classes;
+    }
+    add_filter('nav_menu_css_class', 'nav_add_li_class', 1, 3);
+}
+
+
+
+/**
+ * Function to add args in wp_nav_menu() to add custom class in <a>
+ * 
+ * @since    1.0.0
+ */
+if (!function_exists('nav_add_link_class')) {
+    function nav_add_link_class($atts, $item, $args)
+    {
+        if (property_exists($args, 'link_class')) {
+            $atts['class'] = $args->link_class;
+        }
+        return $atts;
+    }
+    add_filter('nav_menu_link_attributes', 'nav_add_link_class', 1, 3);
+}
+
+
+/**
+ * Function to get nav object
+ * 
+ * @since    1.0.0
+ */
+if (!function_exists('get_menu_by_location')) {
+    function get_menu_by_location($location)
+    {
+        if (empty($location)) return false;
+
+        $locations = get_nav_menu_locations();
+        if (!isset($locations[$location])) return false;
+
+        $menu_obj = get_term($locations[$location], 'nav_menu');
+
+        return $menu_obj;
+    }
+}
---
+++ wp-content/themes/yes-twentytwentyone/template-parts/footer/site-footer.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Displays the site footer.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package yes-twentytwentyone
+ */
+
+?>
+
+<!-- Footer STARTS -->
+<footer class="footer">
+    <div class="container g-lg-0">
+        <div class="row position-relative">
+            <div class="col-12 col-md-2">
+                <img src="/wp-content/themes/yes-twentytwentyone/assets/images/logo.svg" class="footer-logo mb-4" />
+                <div class="row mb-2">
+                    <ul class="icon-list">
+                        <li>
+                            <a href="#"><img src="/wp-content/themes/yes-twentytwentyone/assets/images/facebook.svg" /></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="/wp-content/themes/yes-twentytwentyone/assets/images/instagram.svg" /></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="/wp-content/themes/yes-twentytwentyone/assets/images/youtube.svg" /></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="/wp-content/themes/yes-twentytwentyone/assets/images/twitter.svg" /></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="row">
+                    <p class="mb-2">Get The MyYes App</p>
+                    <ul class="icon-list">
+                        <li>
+                            <a href="#"><img src="/wp-content/themes/yes-twentytwentyone/assets/images/logos_apple-app-store.svg" /></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="/wp-content/themes/yes-twentytwentyone/assets/images/logos_google-play-icon.svg" /></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 col-md-9">
+                <div class="row g-0 row-cols-2 row-cols-sm-1 row-cols-md-5">
+                    <?php 
+                        for ($i = 1; $i <= 5; $i++) :
+                            $menu_location	= "footer-column-$i";
+                    ?>
+                    <div class="col">
+                        <?php 
+                            if (has_nav_menu($menu_location)) :
+                                $menu_obj	= get_menu_by_location($menu_location);
+                                wp_nav_menu(['theme_location' => $menu_location, 'container' => false, 'items_wrap' => '<h3 class="menu-heading">'.esc_html($menu_obj->name).'</h3><ul id="%1$s" class="%2$s menulist">%3$s</ul>']);
+                            endif;
+                        ?>
+                    </div>
+                    <?php
+                        endfor;
+                    ?>
+                </div>
+            </div>
+            <div class="col-12 col-md-1">
+                <img src="/wp-content/themes/yes-twentytwentyone/assets/images/ctm.png" class="img-fluid ctm-logo" />
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 d-flex align-items-center justify-content-md-start justify-content-sm-center">
+                    <p><?php echo esc_html__('POWERED BY YTL, YTL Communications Sdn. Bhd. (793634-V)', 'yes.my'); ?></p>
+                </div>
+                <div class="col-12 col-md-6 d-flex align-items-center justify-content-md-end justify-content-sm-center">
+                    <img src="/wp-content/themes/yes-twentytwentyone/assets/images/footer-icons.png" class="img-fluid" />
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer ENDS -->
