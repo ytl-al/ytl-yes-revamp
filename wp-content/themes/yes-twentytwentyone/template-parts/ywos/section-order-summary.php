eb0a4509540c7875c67883bffae06d113b1d41a4
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2022-04-14 15:07:20 +0800

    YWOS Bug Fix - LYR-1683 - Postpaid - Alignment issue in bundle plan Review and pay page - Order Summary section

--- wp-content/themes/yes-twentytwentyone/template-parts/ywos/section-order-summary.php
+++ wp-content/themes/yes-twentytwentyone/template-parts/ywos/section-order-summary.php
@@ -4,18 +4,18 @@
         <template v-for="item in orderSummary.due.priceBreakdown.simplified">
             <template v-if="item.parentFlag">
                 <div class="row" v-if="item.displayOrder > 1">
-                    <div class="col-8 pe-0 ">
+                    <div class="col-7 pe-0 ">
                         <p class="large">Add-Ons</p>
                     </div>
-                    <div class="col-4 text-end">
+                    <div class="col-5 text-end">
                         <p class="large"><strong>RM{{ parseFloat(orderSummary.due.addOns).toFixed(2) }}</strong></p>
                     </div>
                 </div>
                 <div class="row" v-bind:class="{ 'mt-2': item.displayOrder > 1 }">
-                    <div class="col-8 pe-0 py-2">
+                    <div class="col-7 pe-0 py-2">
                         <p style="color: #00B4F0;">{{ item.priceDisplayName }}</p>
                     </div>
-                    <div class="col-4 py-2 text-end">
+                    <div class="col-5 py-2 text-end">
                         <p class="large" style="color: #00B4F0;" v-if="deliveryInfo.securityType == '' || deliveryInfo.securityType == 'NRIC'"><strong>{{ item.malaysianPriceValue }}</strong></p>
                         <p class="large" style="color: #00B4F0;" v-if="deliveryInfo.securityType != '' && deliveryInfo.securityType != 'NRIC'"><strong>{{ item.nonMalaysianPriceValue }}</strong></p>
                     </div>
@@ -23,12 +23,12 @@
             </template>
             <template v-else>
                 <div class="row" v-if="!['Deposit for Foreigner', 'SST @6%', 'Rounding Adjustment'].includes(item.priceDisplayName)">
-                    <div class="col-8 pe-0">
+                    <div class="col-7 pe-0">
                         <p>{{ item.priceDisplayName }}</p>
                     </div>
-                    <div class="col-4 text-end">
-                        <p class="large" v-if="deliveryInfo.securityType == '' || deliveryInfo.securityType == 'NRIC'"><strong>{{ item.malaysianPriceValue.replace('(-1)', '(-) ') }}</strong></p>
-                        <p class="large" v-if="deliveryInfo.securityType != '' && deliveryInfo.securityType != 'NRIC'"><strong>{{ item.nonMalaysianPriceValue.replace('(-1)', '(-) ') }}</strong></p>
+                    <div class="col-5 text-end">
+                        <p class="large" v-if="deliveryInfo.securityType == '' || deliveryInfo.securityType == 'NRIC'"><strong>{{ item.malaysianPriceValue.replace('(-1)', '(-1) ') }}</strong></p>
+                        <p class="large" v-if="deliveryInfo.securityType != '' && deliveryInfo.securityType != 'NRIC'"><strong>{{ item.nonMalaysianPriceValue.replace('(-1)', '(-1) ') }}</strong></p>
                     </div>
                 </div>
             </template>
@@ -37,10 +37,10 @@
     <template v-else>
         <!-- <h2>Due today after taxes and shipping</h2> -->
         <div class="row">
-            <div class="col-8 py-2">
+            <div class="col-7 py-2">
                 <p style="color: #00B4F0;">Total Payment</p>
             </div>
-            <div class="col-4 py-2 text-end">
+            <div class="col-5 py-2 text-end">
                 <p class="large" style="color: #00B4F0;">RM{{ formatPrice(parseFloat(orderSummary.due.total).toFixed(2)) }}</p>
             </div>
         </div>
