3284f27d8b321d558d4327a528d4f3a620b1d91a
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2022-06-10 14:51:08 +0800

    Removed Google Tag code from previous agency

--- wp-content/themes/yes-twentytwentyone/footer.php
+++ wp-content/themes/yes-twentytwentyone/footer.php
@@ -38,6 +38,28 @@
 					}
 				});
 			}
+
+			$('.btn-buyplan').on('click', function() {
+				dataLayer.push({ ecommerce: null });
+				dataLayer.push({
+					'event': 'addToCart',
+					'ecommerce': {
+						'currencyCode': 'MYR',
+						'add': {                                // 'add' actionFieldObject measures.
+						'products': [{                        //  adding a product to a shopping cart.
+							'name': 'Triblend Android T-Shirt 2',
+							'id': '12345',
+							'price': '15.25',
+							'brand': 'Google',
+							'category': 'Apparel',
+							'variant': 'Gray',
+							'quantity': 1
+						}]
+						}
+					}
+				});
+				alert('asd');
+			});
 		});
 	</script>
 
--- wp-content/themes/yes-twentytwentyone/header-ywos.php
+++ wp-content/themes/yes-twentytwentyone/header-ywos.php
@@ -44,14 +44,6 @@
 	</script> -->
 	<!-- END Global site tag (gtag.js) - Google Analytics -->
 
-	<!-- Google Tag Manager -->
-	<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-T8K5HSR');</script> -->
-	<!-- END Google Tag Manager -->
-
 	<!-- Facebook Pixel Code -->
 	<!-- <script>
 		!function(f,b,e,v,n,t,s)
@@ -73,9 +65,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<!-- Google Tag Manager (noscript) -->
-	<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8K5HSR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
-	<!-- End Google Tag Manager (noscript) -->
 
 	<?php wp_body_open(); ?>
 
