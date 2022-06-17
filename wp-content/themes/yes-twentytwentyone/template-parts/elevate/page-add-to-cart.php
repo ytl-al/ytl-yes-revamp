5d9debe467927f4bf4c6990631b7d12bad5580bf
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2022-06-17 15:26:44 +0800

    Added Analytics on elevate add to cart page

--- wp-content/themes/yes-twentytwentyone/template-parts/elevate/page-add-to-cart.php
+++ wp-content/themes/yes-twentytwentyone/template-parts/elevate/page-add-to-cart.php
@@ -1,7 +1,6 @@
 <?php require_once('includes/header.php') ?>
 <script type="text/javascript" src="https://cdn.yes.my/site/wp-content/themes/yes-twentytwentyone/template-parts/elevate/assets/js/elevate.js "></script>
 <script type="text/javascript">
-
     $(document).ready(function () {
 		var url_string = window.location.href;
 		var url = new URL(url_string);
@@ -11,3 +10,4 @@
 		}
 	});
 </script>
+<?php require_once('includes/footer.php'); ?>
