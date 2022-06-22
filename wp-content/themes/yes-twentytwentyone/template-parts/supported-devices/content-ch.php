f7eb5d05ffb903426e55b5361e9ad4a5c65e0e19
zrasheed1983 <zrasheed@gmail.com>
2022-06-22 11:03:31 +0800

    Updated FAQs

--- wp-content/themes/yes-twentytwentyone/template-parts/supported-devices/content-ch.php
+++ wp-content/themes/yes-twentytwentyone/template-parts/supported-devices/content-ch.php
@@ -35,7 +35,8 @@
     </div>
 </section>
 
-<section id="faq-section">
+<!-- Footer FAQs STARTS -->
+<section class="layer-footerFAQ mt-4 mt-lg-0" id="faq-section" data-aos="fade-up">
     <div class="container">
         <div class="row">
             <h1 class="mb-5">Frequently Asked Questions</h1>
@@ -44,40 +45,35 @@
             <div class="col-12 col-lg-9">
                 <div class="accordion accordion-flush mb-3" id="accordionFlushExample">
                     <div class="accordion-item">
-                        <h2 class="accordion-header" id="flush-headingOne">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">YES Mobile Number Portability</button>
-                        </h2>
+                        <h2 class="accordion-header" id="flush-headingOne"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">How do I keep my number?</button></h2>
                         <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                            <div class="accordion-body">
+                                <p>Switch To Yes without changing your number, visit <a href="https://www.yes.my/docs/faq/switch-to-yes/yes-mobile-number-portability-prepaid/">here</a> for more info.</p>
+                            </div>
                         </div>
                     </div>
                     <div class="accordion-item">
-                        <h2 class="accordion-header" id="flush-headingTwo">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">YES ID, YES Number &amp; Password</button>
-                        </h2>
+                        <h2 class="accordion-header" id="flush-headingTwo"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">Where are the Yes 5G coverage areas in Malaysia?</button></h2>
                         <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                            <div class="accordion-body">
+                                <p>To check out our 5G coverage areas, you can visit <a href="https://www.yes.my/coverage/">here</a>.</p>
+                            </div>
                         </div>
                     </div>
                     <div class="accordion-item">
-                        <h2 class="accordion-header" id="flush-headingThree">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">Billing &amp; Payment</button>
-                        </h2>
+                        <h2 class="accordion-header" id="flush-headingThree"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">Is there any contract period if I subscribe to Yes Infinite Postpaid Service Plans?</button></h2>
                         <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but
-                                just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.
+                            <div class="accordion-body">
+                                <p>There is no contract for Yes Infinite Postpaid Plans.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <p class="text-center mb-0">
-                    <a href="/faq" class="viewall-btn">View All FAQ <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--akar-icons" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" data-icon="akar-icons:arrow-right">
-                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12h16m-7-7l7 7l-7 7"></path>
-                    </svg></a>
-                </p>
+                <p class="text-center mb-0"><a href="/faq" class="viewall-btn">View All FAQs <span class="iconify" data-icon="akar-icons:arrow-right"></span></a></p>
             </div>
         </div>
     </div>
 </section>
+<!-- Footer FAQs ENDS -->
 
 <?php get_template_part('template-parts/supported-devices/scripts'); ?>
