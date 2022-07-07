c0513db04f6b324a7b719fa5069d90a2f7abcf53
Luca <dtluc@cmcglobal.vn>
2022-07-07 17:35:05 +0700

    update elevate translate

---
+++ wp-content/themes/yes-twentytwentyone/template-parts/elevate/assets/js/elevate_lang.js
@@ -0,0 +1,87 @@
+var Elevate_lang = [];
+Elevate_lang['en-US'] = [];
+Elevate_lang['ms-MY'] = [];
+Elevate_lang['zh-hans'] = [];
+
+Elevate_lang['en-US']['back'] = 'Back';
+Elevate_lang['en-US']['back_to_cart'] = 'Back to Cart';
+Elevate_lang['en-US']['back_to_infinite'] = 'Back to Infinite+';
+Elevate_lang['en-US']['continue'] = 'Continue';
+Elevate_lang['en-US']['help'] = 'Help';
+Elevate_lang['en-US']['items'] = 'Items';
+Elevate_lang['en-US']['error'] = 'Error';
+Elevate_lang['en-US']['capacity'] = 'Capacity';
+Elevate_lang['en-US']['plan'] = 'Plan';
+Elevate_lang['en-US']['select_color'] = 'Select color';
+Elevate_lang['en-US']['select_contract_type'] = 'Select contract type';
+Elevate_lang['en-US']['order_summary'] = 'Order summary';
+Elevate_lang['en-US']['due_today'] = 'DUE TODAY';
+Elevate_lang['en-US']['tax_rounding'] = 'Tax(SST) + Rounding Adjustment';
+Elevate_lang['en-US']['monthly_charges'] = 'Monthly Charges';
+Elevate_lang['en-US']['price_uncludsive_of_sales'] = '*Price is not uncludsive of Sales & Service Tax(SST)';
+Elevate_lang['en-US']['payment_duration'] = 'Payment Duration';
+Elevate_lang['en-US']['administration_payment'] = 'Administration Payment';
+Elevate_lang['en-US']['monthly_payment'] = 'Monthly Payment';
+Elevate_lang['en-US']['total'] = 'TOTAL';
+Elevate_lang['en-US']['check_out'] = 'Check out';
+Elevate_lang['en-US']['no_item_in_cart'] = 'No item in the cart';
+Elevate_lang['en-US']['browse_plan_here'] = 'You may browse the plans available <a href="/infinite-phone-bundles/">here</a>.';
+Elevate_lang['en-US']['payment_note'] = 'The Monthly Instalment payment amount generated is just an estimate. To confirm the exact amount. Kindly get in touch with Maybank.';
+
+Elevate_lang['en-US']['elevate_step_1'] = '1. Eligibility check';
+Elevate_lang['en-US']['elevate_step_2'] = '2. MyKAD verification';
+Elevate_lang['en-US']['elevate_step_3'] = '3. Delivery details';
+Elevate_lang['en-US']['elevate_step_4'] = '4. Review and order';
+
+Elevate_lang['en-US']['if_you_are_eligible'] = 'Check if you are eligible for the Yes Infinite+ contract option';
+Elevate_lang['en-US']['eligibility_check'] = 'Eligibility Check';
+Elevate_lang['en-US']['fill_in_mykad'] = 'Please fill in your MyKad/MyTentera information, mobile<br> number and email to proceed';
+Elevate_lang['en-US']['MyKAD_verification'] = 'MyKAD Verification';
+Elevate_lang['en-US']['MyKAD_number'] = '* MyKad/MyTentera number';
+Elevate_lang['en-US']['MyKad'] = 'MyKad';
+Elevate_lang['en-US']['MyTentera'] = 'MyTentera';
+Elevate_lang['en-US']['full_name'] = '* Full Name (as per MyKad)';
+Elevate_lang['en-US']['date_of_birth'] = '* Date Of Birth';
+Elevate_lang['en-US']['mobile_number'] = '* Key in your mobile number';
+Elevate_lang['en-US']['email_address'] = '* Email address';
+Elevate_lang['en-US']['term_and_condition1'] = 'I here by agree to subscribe to the plan selected in the online form submitted by me, and to be bound by the First to 5G Campaign Terms and Conditions available at <a target="_blank" href="https://www.yes.my/tnc/ongoing-campaigns-tnc">www.yes.my/tnc/ongoing-campaigns-tnc</a>.';
+Elevate_lang['en-US']['term_and_condition2'] = 'I further give consent to YTLC to process my personal data in accordance with YTL Group Privacy Policy available at <a href="https://www.ytl.com/privacypolicy.asp" target="_blank">https://www.ytl.com/privacypolicy.asp</a> and also give consent to TOP to process my personal data in accordance with TOP Privacy Policy available at (<a href="http://yes.compasia.com/TOP_PRIVACY_POLICY.PDF" target="_blank">http://yes.compasia.com/TOP_PRIVACY_POLICY.PDF</a>) for the purposes of my agreement with TOP.';
+Elevate_lang['en-US']['check_eligibility'] = 'Check Eligibility';
+Elevate_lang['en-US']['phone_number'] = 'Phone number';
+Elevate_lang['en-US']['compasia_fail_error1'] = 'Sorry! We’ve checked and you currently do not qualify for the Yes Infinite+ contract option';
+Elevate_lang['en-US']['compasia_fail_error2'] = 'Sorry, you do not qualify for the Yes Infinite+ contract option, please select another contract option:';
+
+Elevate_lang['en-US']['eligibility_check_unsuccessful'] = 'Eligibility check unsuccessful';
+Elevate_lang['en-US']['EKYC_check_unsuccessful'] = 'EKYC check unsuccessful';
+Elevate_lang['en-US']['page_error_label1'] = 'Unfortunately, we could not confirm your eligibility for the Yes Infinite+ plan online. <br>Please either retry in an hour or proceed to our <a target="_blank" href="https://www.yes.my/support/store-locator/">Yes Stores</a> for further assistance.';
+Elevate_lang['en-US']['page_error_label2'] = 'Unfortunately, we could not verify your identity online.<br> Please either retry and ensure that you are following the ekyc steps or proceed to our <a target="_blank" href="https://www.yes.my/support/store-locator/">Yes Stores</a> for further assistance.';
+Elevate_lang['en-US']['retry'] = 'Retry';
+Elevate_lang['en-US']['yes_store_location'] = 'Yes Store Locations';
+
+Elevate_lang['en-US']['MyKAD_verification'] = 'MyKAD Verification';
+Elevate_lang['en-US']['verify_label_1'] = 'A few steps to verify your identity<br> before we continue.';
+Elevate_lang['en-US']['verify_label_2'] = 'Scan the QR code to begin verification';
+Elevate_lang['en-US']['verify_now'] = 'Verify Now';
+Elevate_lang['en-US']['verify_label_3'] = 'Complete the verification in 2 simple steps!';
+Elevate_lang['en-US']['verify_label_4'] = 'MyKAD Validation';
+Elevate_lang['en-US']['verify_label_5'] = 'Scan your MyKAD with the object in a well lit room facing on a flat surface with minimum reflection';
+Elevate_lang['en-US']['verify_label_6'] = 'Face Verification';
+Elevate_lang['en-US']['verify_label_7'] = 'Ensure your face is within the frame for an accurate detection';
+
+Elevate_lang['en-US']['personal_details'] = 'Personal Details';
+Elevate_lang['en-US']['delivery_only_available_in_malaysia'] = 'Delivery only available in Malaysia.<br> Ensure all information is correct before proceeding.';
+Elevate_lang['en-US']['alternate_name_person'] = '* Name of <br>Alternate Contact Person';
+Elevate_lang['en-US']['alternate_contact_person'] = '* Phone Number of<br> Alternate Contact Person';
+Elevate_lang['en-US']['address'] = '* Address';
+Elevate_lang['en-US']['unit_number'] = 'Unit number';
+Elevate_lang['en-US']['postcode'] = '* Postcode';
+Elevate_lang['en-US']['state'] = '* State';
+Elevate_lang['en-US']['city'] = '* City';
+
+Elevate_lang['en-US']['review_and_pay'] = 'Review & Pay';
+
+
+Elevate_lang['ms-MY']['capacity'] = 'Capacity';
+
+
+Elevate_lang['zh-hans']['capacity'] = 'Capacity';
