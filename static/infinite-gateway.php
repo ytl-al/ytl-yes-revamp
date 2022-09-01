68c42939aeffeee6f84f8c7b4a5090c52d382380
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2022-09-01 14:01:04 +0800

    Infinite gateway static Merged

--- static/infinite-gateway.php
+++ static/infinite-gateway.php
@@ -12,34 +12,52 @@
     #best-solution .layer-solutionBox h2 {font-size:26px; color:#000; font-weight:800;}
     #best-solution .layer-solutionBox h3 {font-size:16px; color:#000; font-weight:400;}
 
-    .layer-comparison {}
+    .layer-comparison { padding: 60px 0; text-align: center; }
     .layer-comparison h3 {}
+    .layer-comparison .layer-gradientText p { font-size: 25px; }
     .layer-gradientText {}
-    .layer-gradientText h3, .layer-gradientText p {}
+    .layer-gradientText h3, .layer-gradientText p { background: linear-gradient(80.9deg, #FF0084 16.48%, #6F29D2 85.6%, #2F3BF5 96.9%); -webkit-background-clip: text; -webkit-text-fill-color: transparent; background-clip: text; text-fill-color: transparent; }
     .layer-comparison p {}
-    .layer-comparison .layer-contentBox {}
-    .layer-contentBox.box-gradient {}
+    .layer-comparison .layer-contentBox { background-color: #F2F2F2; border-radius: 5px; padding: 25px; }
+    .layer-contentBox.box-gradient { background: linear-gradient(45.9deg, #FF0084 16.48%, #6F29D2 55.6%, #2F3BF5 96.9%); }
+    .layer-contentBox p { font-size: 18px; margin: 8px 0; }
+    .layer-contentBox .span-hideLarge { margin: 0 12px 0 0; }
+    .layer-contentBox .panel-lineHeightDouble { line-height: normal; }
+    .layer-contentBox.box-gradient p { color: #FFF; }
 
-    .layer-steps {}
+    .layer-steps { padding: 60px 0; }
+    .layer-steps h2 { color: #000; font-size: 39px; font-weight: 800; line-height: 47px; }
     .layer-steps .layer-step {}
-    .layer-step .panel-stepHeading {}
+    .layer-step .panel-stepHeading { font-size: 25px; font-weight: 800; text-transform: uppercase; }
     .layer-step p {}
 
-    .layer-sectionGradient {}
-    .layer-plans {}
-    .layer-plans h2 {}
+    .layer-sectionGradient { background: linear-gradient(80.9deg, #FF0084 16.48%, #6F29D2 85.6%, #2F3BF5 96.9%); }
+    .layer-plans { padding: 60px 0; }
+    .layer-plans h2 { color: #FFF; font-size: 39px; font-weight: 800; line-height: 47px; }
     .layer-plans p {}
+    .layer-plans .row-sectionHeader h2, .layer-plans .row-sectionHeader p { color: #FFF; }
+    .layer-plans .row-sectionHeader p { color: #FFF; font-size: 20px; }
 
-    .layer-planBox {}
+    .layer-planBox { background-color: transparent; border: 0; }
+    .layer-planBox .card-body { background-color: #FFF; border-radius: 20px 20px 0 0; padding: 20px 30px; }
+    .layer-planBox .card-body p { font-size: 22px; }
     .layer-planBox h3 {}
     .layer-planBox p {}
-    .layer-planBox hr {}
-    .layer-planBox ul {}
+    .layer-planBox hr { background-color: #000; opacity: 1; }
+    .layer-planBox ul { margin: 0; padding: 0 0 0 20px; }
     .layer-planBox ul li {}
-    .layer-planBox .panel-price {}
-    .layer-planBox .panel-price .span-large {}
-    .layer-planBox .panel-tnc {}
-    .layer-planBox .panel-btnBuy a {}
+    .layer-planBox .card-footer { background-color: #F4F4F4; border-radius: 0 0 20px 20px; margin: 0 0 30px; text-align: center; }
+    .layer-planBox .card-footer p { font-size: 20px; }
+    .layer-planBox .panel-price { font-weight: 800; }
+    .layer-planBox .panel-price .span-large { font-size: 30px; }
+    .layer-planBox .panel-tnc { font-size: 10px !important; }
+    .layer-plans p.panel-btnBuy { text-align: center; }
+    .layer-plans p.panel-btnBuy a {}
+
+    @media (min-width: 992px) {
+        .layer-contentBox .panel-lineHeightDouble { line-height: 54px; }
+        .span-hideLarge { display: none; }
+    }
 </style>
 
 <main class="clearfix site-main" id="primary" role="main">
@@ -144,43 +162,50 @@
             <section id="section-comparison" data-aos="fade-up" data-aos-duration="1000" class="layer-comparison aos-init aos-animate">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-4 offset-lg-4">
-                            <div class="layer-gradientText">
-                                <h3>Infinite Gateway</h3>
-                                <p>(Wireless Fibre)</p>
+                        <div class="col-lg-10 offset-lg-1">
+                            <div class="row align-items-center mb-3">
+                                <div class="col-lg-4 offset-lg-4 py-2">
+                                    <div class="layer-gradientText">
+                                        <h3>Infinite Gateway</h3>
+                                        <p>(Wireless Fibre)</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 py-2 d-none d-lg-block">
+                                    <h3>Wired Fibre</h3>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <h3>Wired Fibre</h3>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-4 text-center">
-                            <p>Speed</p>
-                            <p>Bands</p>
-                            <p>Max number of devices connectable</p>
-                            <p>Coverage range</p>
-                            <p>Installation</p>
-                            <p>Moveable from room-to-room</p>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="layer-contentBox box-gradient">
-                                <p>5G (Up to xxMbpx)</p>
-                                <p>2.5GHz or 5GHz</p>
-                                <p>32</p>
-                                <p>Up to XXXX sq. ft.</p>
-                                <p>No</p>
-                                <p>Yes</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="layer-contentBox">
-                                <p>4G (Up to xxMbpx)</p>
-                                <p>2.5GHz or 5GHz</p>
-                                <p>xx</p>
-                                <p>Up to XXXX sq. ft.</p>
-                                <p>Yes</p>
-                                <p>No</p>
+                            <div class="row">
+                                <div class="col-lg-4 text-center d-none d-lg-block">
+                                    <div class="layer-contentBox bg-white">
+                                        <p>Speed</p>
+                                        <p>Bands</p>
+                                        <p>Max number of <br />devices connectable</p>
+                                        <p>Coverage range</p>
+                                        <p>Installation</p>
+                                        <p>Moveable from <br />room-to-room</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 mb-4 mb-lg-0">
+                                    <div class="layer-contentBox box-gradient">
+                                        <p><span class="span-hideLarge">Speed:</span>5G (Up to xxMbpx)</p>
+                                        <p><span class="span-hideLarge">Bands:</span>2.5GHz or 5GHz</p>
+                                        <p class="panel-lineHeightDouble"><span class="span-hideLarge">Max number of devices connectable:</span>32</p>
+                                        <p><span class="span-hideLarge">Coverage range:</span>Up to XXXX sq. ft.</p>
+                                        <p><span class="span-hideLarge">Installation:</span>No</p>
+                                        <p class="panel-lineHeightDouble mb-0"><span class="span-hideLarge">Moveable from room-to-room:</span>Yes</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <h3 class="mb-3 d-lg-none">Wired Fibre</h3>
+                                    <div class="layer-contentBox">
+                                        <p><span class="span-hideLarge">Speed:</span>4G (Up to xxMbpx)</p>
+                                        <p><span class="span-hideLarge">Bands:</span>2.5GHz or 5GHz</p>
+                                        <p class="panel-lineHeightDouble"><span class="span-hideLarge">Max number of devices connectable:</span>xx</p>
+                                        <p><span class="span-hideLarge">Coverage range:</span>Up to XXXX sq. ft.</p>
+                                        <p><span class="span-hideLarge">Installation:</span>Yes</p>
+                                        <p class="panel-lineHeightDouble mb-0"><span class="span-hideLarge">Moveable from room-to-room:</span>No</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -210,6 +235,13 @@
 
             <!-- Layer Steps STARTS -->
             <section class="layer-steps" id="section-steps">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h2 class="text-center">Here's how to get it.</h2>
+                        </div>
+                    </div>
+                </div>
                 <div class="container my-5">
                     <div class="row gx-5 text-md-start text-center">
                         <div class="col-12 col-md-3 layer-step flex-column aos-init aos-animate" data-aos="fade-up" data-aos-duration="500" data-aos-delay="100">
@@ -236,15 +268,15 @@
             <!-- Layer Plans STARTS -->
             <div class="layer-sectionGradient layer-plans aos-init aos-animate" id="infinite-wireless-fibre-plans" data-aos="fade-up" data-aos-duration="1000">
                 <div class="container">
-                    <div class="row">
+                    <div class="row mb-5 row-sectionHeader">
                         <div class="col-lg-12 text-center">
                             <h2>Wireless Fibre Plans</h2>
                             <p>Unlock infinite possibilities in the comfort of your home.</p>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-10 offset-lg-1">
-                            <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <div class="col-lg-8 offset-lg-2">
+                            <div class="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
                                 <div class="col">
                                     <div class="layer-planBox card h-100">
                                         <div class="card-body">
@@ -263,8 +295,8 @@
                                             <p>120Mbps at</p>
                                             <p class="panel-price"><span class="span-large">RM119</span> /mth</p>
                                             <p class="panel-tnc">*Terms and conditions apply</p>
-                                            <p class="panel-btnBuy"><a href="javascript:void(0)" class="btn pink-btn btn-buyplan">Buy Now</a></p>
                                         </div>
+                                    <p class="panel-btnBuy"><a href="javascript:void(0)" class="btn pink-btn btn-buyplan">Buy Now</a></p>
                                     </div>
                                 </div>
                                 <div class="col">
@@ -286,8 +318,8 @@
                                             <p>120Mbps at</p>
                                             <p class="panel-price"><span class="span-large">RM148</span> /mth</p>
                                             <p class="panel-tnc">*Terms and conditions apply</p>
-                                            <p class="panel-btnBuy"><a href="javascript:void(0)" class="btn pink-btn btn-buyplan">Buy Now</a></p>
                                         </div>
+                                    <p class="panel-btnBuy"><a href="javascript:void(0)" class="btn pink-btn btn-buyplan">Buy Now</a></p>
                                     </div>
                                 </div>
                             </div>
