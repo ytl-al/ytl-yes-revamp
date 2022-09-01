6d89b54b262248e3624fb3c06c24d42fa1d5299c
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2022-09-01 12:57:00 +0800

    Infinite Gateway Page - Skeletons

---
+++ static/infinite-gateway.php
@@ -0,0 +1,298 @@
+<?php include('templates/header.php'); ?>
+
+<style type="text/css">
+    .layer-solutions {}
+    .layer-solutions .layer-solutionBox {}
+    .layer-solutionBox img {}
+    .layer-solutionBox h2 {}
+
+    .layer-comparison {}
+    .layer-comparison h3 {}
+    .layer-gradientText {}
+    .layer-gradientText h3, .layer-gradientText p {}
+    .layer-comparison p {}
+    .layer-comparison .layer-contentBox {}
+    .layer-contentBox.box-gradient {}
+
+    .layer-steps {}
+    .layer-steps .layer-step {}
+    .layer-step .panel-stepHeading {}
+    .layer-step p {}
+
+    .layer-sectionGradient {}
+    .layer-plans {}
+    .layer-plans h2 {}
+    .layer-plans p {}
+
+    .layer-planBox {}
+    .layer-planBox h3 {}
+    .layer-planBox p {}
+    .layer-planBox hr {}
+    .layer-planBox ul {}
+    .layer-planBox ul li {}
+    .layer-planBox .panel-price {}
+    .layer-planBox .panel-price .span-large {}
+    .layer-planBox .panel-tnc {}
+    .layer-planBox .panel-btnBuy a {}
+</style>
+
+<main class="clearfix site-main" id="primary" role="main">
+    <!-- Article STARTS -->
+    <article>
+        <!-- Entry Content STARTS -->
+        <div class="entry-content">
+
+            <!-- Breadcrumb STARTS -->
+            <div class="layer-breadcrumb">
+                <div class="container breadcrumb-section">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item active"><a href="/static/infinite-phone-bundle.php">Infinite Wireless Fibre</a></li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <!-- Breadcrumb ENDS -->
+
+            <!-- Slider Start -->
+            <section id="infinity-banner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-8 col-md-8 col-lg-12 col-xl-6 d-flex align-items-start align-items-lg-center justify-content-start" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                            <div>
+                                <img src="https://cdn.yes.my/site/wp-content/uploads/2022/07/infinite-bundle-banner-infinite-new.png" alt="" class="mb-2 mb-lg-2">
+                                <img src="https://cdn.yes.my/site/wp-content/uploads/2022/05/infinite-bundle-banner-price.png" alt="" class="mb-2 d-none d-lg-block">
+                                <div class="row">
+                                    <div class="col-lg-5">
+                                        <p><img src="https://cdn.yes.my/site/wp-content/uploads/2022/03/yes-logo-new-white.png" width="35" /></p>
+                                        <p class="panel-bannerIG">Infinite Gateway <img src="/static/images/infinite-gateway/icon-5g.png" /></p>
+                                        <h2>Home Fibre is now Wireless.</h2>
+                                        <p class="panel-btn"><a href="javascript:void(0)" class="btn link-jumpSection pink-btn" data-targetsection="infinite-wireless-fibre-plans">Get It Now</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Slider End -->
+
+            <!-- Solution STARTS -->
+            <section id="best-solution" class="layer-solutions py-5">
+                <div class="container">
+                    <h1 class="mb-1 text-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000">The best solution to all your home fibre issues.</h1>
+                    <p class="mb-5 text-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000">Not in Fibre coverage? Fibre Port Full? <br />Home Fibre not fast enough? We've got just the wireless solution for you.​</p>
+                    <div class="row aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="col-12 col-xl-3 col-lg-6 col-md-6">
+                            <div class="layer-solutionBox">
+                                <div class="mb-auto">
+                                    <img src="/static/images/infinite-gateway/icon-solution-7-days.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>FREE router with 7 days trial.</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-xl-3 col-lg-6 col-md-6 mt-4 mt-lg-0">
+                            <div class="layer-solutionBox">
+                                <div class="mb-auto">
+                                    <img src="/static/images/infinite-gateway/icon-solution-fastest.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>Fastest broadband in Malaysia.</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-xl-3 col-lg-6 col-md-6 mt-4 mt-lg-0">
+                            <div class="layer-solutionBox">
+                                <div class="mb-auto">
+                                    <img src="/static/images/infinite-gateway/icon-solution-plug-play.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>Plug & Use.</h2>
+                                    <p>No installation required</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-xl-3 col-lg-6 col-md-6 mt-4 mt-lg-0">
+                            <div class="layer-solutionBox">
+                                <div class="mb-auto">
+                                    <img src="/static/images/infinite-gateway/icon-solution-wireless.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>Wireless & Portable.</h2>
+                                    <p>Use it anywhere, anytime</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Solution ENDS -->
+
+            <!-- Layer Comparison STARTS -->
+            <section id="section-comparison" data-aos="fade-up" data-aos-duration="1000" class="layer-comparison aos-init aos-animate">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 offset-lg-4">
+                            <div class="layer-gradientText">
+                                <h3>Infinite Gateway</h3>
+                                <p>(Wireless Fibre)</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Wired Fibre</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 text-center">
+                            <p>Speed</p>
+                            <p>Bands</p>
+                            <p>Max number of devices connectable</p>
+                            <p>Coverage range</p>
+                            <p>Installation</p>
+                            <p>Moveable from room-to-room</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="layer-contentBox box-gradient">
+                                <p>5G (Up to xxMbpx)</p>
+                                <p>2.5GHz or 5GHz</p>
+                                <p>32</p>
+                                <p>Up to XXXX sq. ft.</p>
+                                <p>No</p>
+                                <p>Yes</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="layer-contentBox">
+                                <p>4G (Up to xxMbpx)</p>
+                                <p>2.5GHz or 5GHz</p>
+                                <p>xx</p>
+                                <p>Up to XXXX sq. ft.</p>
+                                <p>Yes</p>
+                                <p>No</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Layer Comparison ENDS -->
+
+            <!-- Layer Infinite Gateway Banner STARTS -->
+            <section id="infinite-banner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-8 d-flex align-items-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
+                            <div>
+                                <img src="/static/images/infinite-gateway/bg-infinite-banner.png" class="mb-3" alt="">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <p class="panel-sub">Infinite Gateway <img src="/static/images/infinite-gateway/icon-5g.png" /></p>
+                                        <h2>The first ever wireless 5G home fibre in Malaysia.</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Layer Infinite Gateway Banner ENDS -->
+
+            <!-- Layer Steps STARTS -->
+            <section class="layer-steps" id="section-steps">
+                <div class="container my-5">
+                    <div class="row gx-5 text-md-start text-center">
+                        <div class="col-12 col-md-3 layer-step flex-column aos-init aos-animate" data-aos="fade-up" data-aos-duration="500" data-aos-delay="100">
+                            <p class="panel-stepHeading justify-content-md-start justify-content-center"><span class="icon-stepHeading"></span> Step 1</p>
+                            <p>Choose Infinite Gateway Plan.</p>
+                        </div>
+                        <div class="col-12 col-md-3 layer-step flex-column aos-init aos-animate" data-aos="fade-up" data-aos-duration="500" data-aos-delay="300">
+                            <p class="panel-stepHeading justify-content-md-start justify-content-center"><span class="icon-stepHeading"></span> Step 2</p>
+                            <p>Check 5G+4G LTE & broadband coverage.</p>
+                        </div>
+                        <div class="col-12 col-md-3 layer-step flex-column aos-init aos-animate" data-aos="fade-up" data-aos-duration="500" data-aos-delay="500">
+                            <p class="panel-stepHeading justify-content-md-start justify-content-center"><span class="icon-stepHeading"></span> Step 3</p>
+                            <p>Fill up delivery form and check out.</p>
+                        </div>
+                        <div class="col-12 col-md-3 layer-step flex-column aos-init aos-animate" data-aos="fade-up" data-aos-duration="500" data-aos-delay="700">
+                            <p class="panel-stepHeading justify-content-md-start justify-content-center"><span class="icon-stepHeading"></span> Step 4</p>
+                            <p>FREE router & SIM to be delivered to your address.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Layer Steps ENDS -->
+
+            <!-- Layer Plans STARTS -->
+            <div class="layer-sectionGradient layer-plans aos-init aos-animate" id="infinite-wireless-fibre-plans" data-aos="fade-up" data-aos-duration="1000">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <h2>Wireless Fibre Plans</h2>
+                            <p>Unlock infinite possibilities in the comfort of your home.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-10 offset-lg-1">
+                            <div class="row row-cols-1 row-cols-md-3 g-4">
+                                <div class="col">
+                                    <div class="layer-planBox card h-100">
+                                        <div class="card-body">
+                                            <h3>Wireless 5G+4G <br />Home Fibre</h3>
+                                            <p>Standalone plan</p>
+                                            <hr />
+                                            <ul>
+                                                <li>Unlimited 5G data</li>
+                                                <li>150GB 4G data</li>
+                                                <li>FREE Router (RRP: <span class="span-slash">RM1,199</span></li>
+                                                <li>7 days free trial</li>
+                                                <li>24-month contract</li>
+                                            </ul>
+                                        </div>
+                                        <div class="card-footer">
+                                            <p>120Mbps at</p>
+                                            <p class="panel-price"><span class="span-large">RM119</span> /mth</p>
+                                            <p class="panel-tnc">*Terms and conditions apply</p>
+                                            <p class="panel-btnBuy"><a href="javascript:void(0)" class="btn pink-btn btn-buyplan">Buy Now</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="layer-planBox card h-100">
+                                        <div class="card-body">
+                                            <h3>Wireless 5G <br />Home Fibre</h3>
+                                            <p>Bundle plan</p>
+                                            <hr />
+                                            <ul>
+                                                <li>Unlimited 5G data</li>
+                                                <li>150GB 4G data</li>
+                                                <li>FREE Router (RRP: <span class="span-slash">RM1,199</span></li>
+                                                <li>7 days free trial</li>
+                                                <li>2 mobile lines with Infinite full speed data & calls</li>
+                                                <li>24-month contract</li>
+                                            </ul>
+                                        </div>
+                                        <div class="card-footer">
+                                            <p>120Mbps at</p>
+                                            <p class="panel-price"><span class="span-large">RM148</span> /mth</p>
+                                            <p class="panel-tnc">*Terms and conditions apply</p>
+                                            <p class="panel-btnBuy"><a href="javascript:void(0)" class="btn pink-btn btn-buyplan">Buy Now</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Layer Plans ENDS -->
+
+            <?php include('templates/footer-faq.php'); ?>
+        </div>
+        <!-- Entry Content ENDS -->
+    </article>
+    <!-- Article ENDS -->
+</main>
+
+<?php include('templates/footer.php'); ?>
