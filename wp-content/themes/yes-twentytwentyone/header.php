ff7c83d5f1bd5a589aa7aefba64082d5393a873b
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2022-10-12 12:18:03 +0800

    Re-added Google Tag code from previous agency

--- wp-content/themes/yes-twentytwentyone/header.php
+++ wp-content/themes/yes-twentytwentyone/header.php
@@ -56,6 +56,14 @@
 	</script> -->
 	<!-- END Global site tag (gtag.js) - Google Ads: 10904758864 -->
 
+	<!-- Google Tag Manager -->
+	<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-T8K5HSR');</script> -->
+	<!-- End Google Tag Manager -->
+
 	<!-- Facebook Pixel Code -->
 	<!-- <script>
 		!function(f,b,e,v,n,t,s)
@@ -77,6 +85,10 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8K5HSR"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
+	<!-- End Google Tag Manager (noscript) -->
 
 	<?php wp_body_open(); ?>
 
