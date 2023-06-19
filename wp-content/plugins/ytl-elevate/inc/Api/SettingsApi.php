88e7e71bbbb47f795652ee76edb92e43f95cae22
Al Latif Bin Mohamad <latif.mohamad@N06680s-MacBook-Pro.local>
2023-06-19 15:06:27 +0800

    Fixed the php8.2 on elevate

--- wp-content/plugins/ytl-elevate/inc/Api/SettingsApi.php
+++ wp-content/plugins/ytl-elevate/inc/Api/SettingsApi.php
@@ -40,10 +40,11 @@ class SettingsApi
 
 	public function AddAdminSubMenu(){
 		foreach( $this->admin_subpages as $subpage ){
+			$function = ($subpage['function']) ?? [];
 		    if(isset($subpage['menu_slug'])){
-                add_submenu_page( $subpage['parent_slug'], $subpage['page_title'], $subpage['menu_title'], $subpage['capability'], $subpage['menu_slug'], $subpage['function'] );
+                add_submenu_page( $subpage['parent_slug'], $subpage['page_title'], $subpage['menu_title'], $subpage['capability'], $subpage['menu_slug'], $function );
             }else{
-                add_submenu_page( $subpage['parent_slug'], $subpage['page_title'], $subpage['menu_title'], $subpage['capability'], $subpage['url'], $subpage['function'] );
+                add_submenu_page( $subpage['parent_slug'], $subpage['page_title'], $subpage['menu_title'], $subpage['capability'], $subpage['url'], $function );
             }
 
 		}
