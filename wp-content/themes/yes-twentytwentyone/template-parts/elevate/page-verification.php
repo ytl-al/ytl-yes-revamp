829fbea9f6d3cfefe8972f93475b3aeed753e0d2
jamtechdev <jamtechba@gmail.com>
2023-12-05 15:10:24 +0530

    Fixed static id's from .js file

--- wp-content/themes/yes-twentytwentyone/template-parts/elevate/page-verification.php
+++ wp-content/themes/yes-twentytwentyone/template-parts/elevate/page-verification.php
@@ -287,6 +287,7 @@
                 },
 
                 OCVerification: function (response) {
+
                     var self = this;
                     var params = {
                         mykad: self.eligibility.mykad,
@@ -298,20 +299,179 @@
                     axios.post(apiEndpointURL_elevate + '/orix-check' + '?nonce='+yesObj.nonce, params)
                         .then((response) => {
                             var data = response.data.data;
-                            console.log(data);
                             if (data.result == 'Success') {
-                                elevate.redirectToPage('select-sim-type');
+                                elevate.redirectToPage('personal');
                             } else {
-                                toggleOverlay(false);
-                                toggleModalAlert('Error',this.renderText('NRIC_is_not_eligible'),"elevate.redirectToPage('/compasia-fail')")
+                                
+                                if (elevate.lsData.product) {
+                                    self.orderSummary.product = elevate.lsData.product;
+                                }
+
+                                if (self.orderSummary.product.selected.productCode) {
+                                //     const mapPlanId = {
+                                //         1097: {
+                                //             planID: 1127,
+                                //             deviceID: 2,
+                                //         },
+                                //         1096: {
+                                //             planID: 1126,
+                                //             deviceID: 2,
+                                //         },
+                                //         1095: {
+                                //             planID: 1125,
+                                //             deviceID: 2,
+                                //         },
+                                //         1101: {
+                                //             planID: 1123,
+                                //             deviceID: 3,
+                                //         },
+                                //         1093: {
+                                //             planID: 1131,
+                                //             deviceID: 4,
+                                //         },
+                                //         1198: {
+                                //             planID: 1196,
+                                //             deviceID: 5,
+                                //         },
+                                //         1218: {
+                                //             planID: 1248,
+                                //             deviceID: 5,
+                                //         },
+                                //         1194: {
+                                //             planID: 1192,
+                                //             deviceID: 6,
+                                //         },
+                                //         1204: {
+                                //             planID: 1234,
+                                //             deviceID: 6,
+                                //         },
+                                //         1202: {
+                                //             planID: 1200,
+                                //             deviceID: 7,
+                                //         },
+                                //         1228: {
+                                //             planID: 1258,
+                                //             deviceID: 7,
+                                //         },
+                                //         1230: {
+                                //             planID: 1260,
+                                //             deviceID: 7,
+                                //         },
+                                //         1232: {
+                                //             planID: 1262,
+                                //             deviceID: 7,
+                                //         },
+                                //         1206: {
+                                //             planID: 1236,
+                                //             deviceID: 8,
+                                //         },
+                                //         1208: {
+                                //             planID: 1238,
+                                //             deviceID: 8,
+                                //         },
+                                //         1210: {
+                                //             planID: 1240,
+                                //             deviceID: 8,
+                                //         },
+                                //         1212: {
+                                //             planID: 1242,
+                                //             deviceID: 9,
+                                //         },
+                                //         1214: {
+                                //             planID: 1244,
+                                //             deviceID: 9,
+                                //         },
+                                //         1216: {
+                                //             planID: 1246,
+                                //             deviceID: 9,
+                                //         },
+                                //         1220: {
+                                //             planID: 1250,
+                                //             deviceID: 10,
+                                //         },
+                                //         1222: {
+                                //             planID: 1252,
+                                //             deviceID: 10,
+                                //         },
+                                //         1224: {
+                                //             planID: 1254,
+                                //             deviceID: 10,
+                                //         },
+                                //         1226: {
+                                //             planID: 1256,
+                                //             deviceID: 10,
+                                //         },
+                                //         1266: {
+                                //             planID: 1270,
+                                //             deviceID: 7,
+                                //         },
+                                //         1264: {
+                                //             planID: 1268,
+                                //             deviceID: 7,
+                                //         },
+                                //         1272: {
+                                //             planID: 1284,
+                                //             deviceID: 8,
+                                //         },
+                                //         1274: {
+                                //             planID: 1286,
+                                //             deviceID: 8,
+                                //         },
+                                //         1276: {
+                                //             planID: 1280,
+                                //             deviceID: 8,
+                                //         },
+                                //         1278: {
+                                //             planID: 1282,
+                                //             deviceID: 8,
+                                //         },
+
+
+                                //     };
+
+                                //     if (mapPlanId[self.orderSummary.product.selected.productCode]) {
+                                //         self.upFrontPlanID = mapPlanId[self.orderSummary.product.selected.productCode].planID;
+                                //         if(self.upFrontPlanID !=''){
+                                //             var data = JSON.parse(localStorage.getItem('yesElevate'))
+                                //             data.meta.isUpFrontPlanAvailable='true';
+                                //             data.meta.upFrontPlanID=self.upFrontPlanID;
+                                //             var upfrontData=JSON.stringify(data)
+                                //             localStorage.setItem('yesElevate',upfrontData)
+                                //             toggleModalAlert('Error',this.renderText('NRIC_is_not_eligible'),"elevate.redirectToPage('eligibility-fail-upfront')");
+                                //         }
+                                //     }else{
+                                        
+                                //          toggleModalAlert('Error',this.renderText('NRIC_is_not_eligible'),"elevate.redirectToPage('eligibility-failure')");
+                                //     }
+                    // console.log(self.mapPlanId);
+                                for (const productCode in self.mapPlanId) {
+                                            if (self.mapPlanId.hasOwnProperty(productCode)) {
+                                                const planInfo = self.mapPlanId[productCode];
+                                                
+                                                // Your existing logic here
+                                                if (planInfo) {
+                                                    self.upFrontPlanID = planInfo.planID;
+                                                    if (self.upFrontPlanID !== '') {
+                                                        var data = JSON.parse(localStorage.getItem('yesElevate'));
+                                                        data.meta.isUpFrontPlanAvailable = 'true';
+                                                        data.meta.upFrontPlanID = self.upFrontPlanID;
+                                                        var upfrontData = JSON.stringify(data);
+                                                        localStorage.setItem('yesElevate', upfrontData);
+                                                        toggleModalAlert('Error', this.renderText('NRIC_is_not_eligible'), "elevate.redirectToPage('eligibility-fail-upfront')");
+                                                    }
+                                                } else {
+                                                    toggleModalAlert('Error', this.renderText('NRIC_is_not_eligible'), "elevate.redirectToPage('eligibility-failure')");
+                                                }
+                                            }
+                                    }
+                                }
                             }
                         })
                         .catch((error) => {
                             toggleOverlay(false);
                             console.log(error);
                         });
-                    },
-                                    redirectYWOS:function (){
+                    },                                  redirectYWOS:function (){
                     var self = this;
                     toggleOverlay();
                     ywos.buyPlan(self.selectedPlan);
