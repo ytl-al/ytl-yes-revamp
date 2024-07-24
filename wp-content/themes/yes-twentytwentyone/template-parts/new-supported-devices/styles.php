1856203bf1380d7c19eb3799879b11cd7f64a81d
hn2003 <hn2003@gmail.com>
2024-07-24 12:34:25 +0530

    supported device ui changes

--- wp-content/themes/yes-twentytwentyone/template-parts/new-supported-devices/styles.php
+++ wp-content/themes/yes-twentytwentyone/template-parts/new-supported-devices/styles.php
@@ -1447,8 +1447,11 @@
 
 .brand-h{
     height: 350px;
-    overflow: auto;
-    overflow-x: hidden;
+    overflow: hidden;
+    /* overflow-x: hidden; */
+}
+.brand-h:hover {
+    overflow-y: scroll;
 }
 .form-check-input:checked[type=radio] {
     background-image: url('data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3e%3cpath fill='none' stroke='%23fff' stroke-linecap='round' stroke-linejoin='round' stroke-width='3' d='M6 10l3 3l6-6'/%3e%3c/svg%3e') !important;
